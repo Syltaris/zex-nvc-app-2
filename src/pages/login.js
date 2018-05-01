bd3381df633bd715c423dd1db0f0c71870cf71a0
Ze Xuan <syltaris@gmail.com>
2018-05-01 15:29:47 +0800

    added input for emails

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,26 +14,43 @@ export default class Login extends React.Component {
             loggedIn: false,
             user: props.user,
             input_userName: '',
+            input_email: '',
             error: false,
             logData: undefined,
             logsDisplay: undefined
         }
 
         this.updateNameInput = this.updateNameInput.bind(this);
+        this.updateEmailInput = this.updateEmailInput.bind(this);
         this.populateLogs = this.populateLogs.bind(this);
     }
 
     componentWillMount() {
+        //track visits
         fetch("http://52.77.251.137:1337/qvlogs", {
-            method: 'GET',
+            body: JSON.stringify({
+                date: new Date(),
+                userId: 'visitor',
+                interactionType: 'visitor'
+            }),
+            method: 'POST',
             headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             //'Authorization': `Bearer `+ jwt
             }
-        })
-        .then(resp => resp.json())
-        .then(respData => this.setState({logData: respData}))
+        });
+
+        // fetch("http://52.77.251.137:1337/qvlogs", {
+        //     method: 'GET',
+        //     headers: {
+        //     'Accept': 'application/json',
+        //     'Content-Type': 'application/json',
+        //     //'Authorization': `Bearer `+ jwt
+        //     }
+        // })
+        // .then(resp => resp.json())
+        // .then(respData => this.setState({logData: respData}))
     }
 
     componentWillReceiveProps(nextProps) {
@@ -56,6 +73,8 @@ export default class Login extends React.Component {
     }
 
     updateNameInput(e) {this.setState({input_userName: e.target.value})}
+    updateEmailInput(e) {this.setState({input_email: e.target.value})}
+
 
     render() {
         return (
@@ -72,7 +91,11 @@ export default class Login extends React.Component {
                         error
                         header="Name cannot be empty!"
                         content="Please at least input something for your name! :("/>
-                        For purposes of this demo, we would only require your name! :)
+                        <Form.Field>
+                            <label>Email</label>
+                            <input type="email" onChange={this.updateEmailInput}/>
+                        </Form.Field>
+                        For purposes of this demo, we would only require your name! If you want to receive early updates for the product you can also leave your email. :) 
                     </Form>
                     <Button onClick={() => {
                         var error = this.state.input_userName.length <= 0;
@@ -86,6 +109,36 @@ export default class Login extends React.Component {
                                 name: this.state.input_userName,
                                 avatarUri: "https://source.unsplash.com/random/20x20"
                             })
+
+                            fetch("http://52.77.251.137:1337/qvlogs", {
+                                body: JSON.stringify({
+                                    date: new Date(),
+                                    userId: this.state.input_userName,
+                                    interactionType: 'sign_in'
+                                }),
+                                method: 'POST',
+                                headers: {
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json',
+                                //'Authorization': `Bearer `+ jwt
+                                }
+                            });
+
+                            if(this.state.input_email && this.state.input_email !== '') {
+                                fetch("http://52.77.251.137:1337/qvlogs", {
+                                    body: JSON.stringify({
+                                        date: new Date(),
+                                        userId: this.state.input_email,
+                                        interactionType: 'email'
+                                    }),
+                                    method: 'POST',
+                                    headers: {
+                                    'Accept': 'application/json',
+                                    'Content-Type': 'application/json',
+                                    //'Authorization': `Bearer `+ jwt
+                                    }
+                                });
+                            }
                         }
                     }}>
                         Submit
